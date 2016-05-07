be3535d90ce1a7b5fcfe972f234b19096c6b3a8f
Seth Berry <seth.berry@nd.edu>
2016-05-06 22:28:12 -0400

    Tweaks and adding map

--- R/initialImportReshape.R
+++ R/initialImportReshape.R
@@ -38,8 +38,19 @@ groupTotals = total %>%
   summarize(tot = sum(n)) %>%
   arrange()
 
+topSpenders = groupTotalsSide %>%
+  filter(perp == "Government of United States of America" |
+           perp == "Government of China" |
+           perp == "Government of Russia (Soviet Union)" |
+           perp == "Government of United Kingdom" |
+           perp == "Government of India" |
+           perp == "Government of Brazil" |
+           perp == "Government of Israel")
+
 as.data.frame(groupTotals) %>%
   select(year, perp, tot) %>%
+  group_by(year, perp) %>%
+  summarize(tot = sum(tot)) %>%
   arrange(year) %>%
   spread(perp, tot) %>%
   dygraph() %>%
@@ -48,4 +59,4 @@ as.data.frame(groupTotals) %>%
               hideOnMouseOut = FALSE) %>%
   dyCSS("dygraphLegend.css")
 
-save(groupTotals, groupTotalsSide, file = "conflictDat.RData")
+save(groupTotals, groupTotalsSide, eventDat, file = "conflictDat.RData")
